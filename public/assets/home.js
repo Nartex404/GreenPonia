fd42b9ec462e0da08cc5820bed5a88941575c608
henrry04 <henrojar_04@hotmail.com>
2023-06-28 13:25:31 -0500

    ajustes de la nueva vista del mapa

--- public/assets/home.js
+++ public/assets/home.js
@@ -1,14 +1,18 @@
 // Initialize and add the map
-const firebaseConfig = {
-    apiKey: "AIzaSyBm2atJDKZpTvpfFUZcVvlqF9yP0LaoKvs",
-    authDomain: "greenponia-b8a97.firebaseapp.com",
-    projectId: "greenponia-b8a97",
-    storageBucket: "greenponia-b8a97.appspot.com",
-    messagingSenderId: "352633184413",
-    appId: "1:352633184413:web:69c661fc0100f9ed20f474",
-    measurementId: "G-CFJZGNQWJ1"
-};
-firebase.initializeApp(firebaseConfig);
+try {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBm2atJDKZpTvpfFUZcVvlqF9yP0LaoKvs",
+        authDomain: "greenponia-b8a97.firebaseapp.com",
+        projectId: "greenponia-b8a97",
+        storageBucket: "greenponia-b8a97.appspot.com",
+        messagingSenderId: "352633184413",
+        appId: "1:352633184413:web:69c661fc0100f9ed20f474",
+        measurementId: "G-CFJZGNQWJ1"
+    };
+    firebase.initializeApp(firebaseConfig);
+} catch (error) {
+    
+}
 
 var btnClose = document.getElementById("btn-close"); //grab the element
 btnClose.onclick = function() {
@@ -26,8 +30,9 @@ function initMap() {
         center: { lat: 4.810266, lng: -74.106988 },
     });
     const db = firebase.firestore();
+    //actualizarFechas(db);
     const locationCollection = db.collection('location');
-    
+
     locationCollection.get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             var latlng = doc.data().mapLocation
@@ -37,7 +42,7 @@ function initMap() {
                 icon: 'images/pin_map_green.png',
                 animation: google.maps.Animation.BOUNCE
             });
-            marker.addListener("click", () => {
+            /*marker.addListener("click", () => {
                 var mapContent = document.getElementById("map");
                 var markerContent = document.getElementById("info-marker");
                 mapContent.style.width = "70%";
@@ -45,15 +50,7 @@ function initMap() {
                 markerContent.style.display = 'block';
                 var titleCrop = document.getElementById("title-crop");
                 titleCrop.innerHTML = doc.data().name;
-                /*var realtime = db.collection("agroclima").doc("realtime");
-                realtime.get().then((doc) => {
-                    var dat = doc.data();
-                    document.getElementById("text-ph").innerHTML = dat.ph;
-                    document.getElementById("text-cond").innerHTML = dat.cond;
-                    document.getElementById("text-hum").innerHTML = dat.hum;
-                    document.getElementById("text-temp").innerHTML = dat.temp;
-                    document.getElementById("text-crec").innerHTML = dat.crec;
-                })*/
+                
                 db.collection("agroclima").doc("realtime")
                 .onSnapshot({
                     // Listen for document metadata changes
@@ -62,14 +59,14 @@ function initMap() {
                     var dat = doc.data();
                     console.log('data',dat)
                     if(dat.timestamp) {
-                        const date = new Date(parseInt(dat.timestamp * 1000));
+                        const date = new Date(parseInt(dat.timestamp));
                         const options = {
                             year: "numeric",
                             month: "long",
                             day: "numeric",
                             hour: "numeric",
                             minute: "numeric",
-                            hour12: true
+                            hour12: false
                         };
                         document.getElementById("text-date").innerHTML = date.toLocaleString("default", options);
                         console.log('timestamp',date.toLocaleString("default", options))
@@ -80,11 +77,29 @@ function initMap() {
                     document.getElementById("text-temp").innerHTML = dat.temp;
                     document.getElementById("text-crec").innerHTML = dat.crec;
                 });
-            })
+            })*/
 
             map.setZoom(12);
+            map.setCenter({lat: latlng._lat, lng: latlng._long})
         });
     });
+}
+
+function actualizarFechas(db) {
+    db.collection('agroclima').get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            var id = doc.id;
+            var data = doc.data();
+            if(id != 'realtime' && id != 'start_system' && !data.hasOwnProperty('timestamp')) {
+                console.log('update agroclima id', id);
+                db.collection('agroclima').doc(id).set({
+                    timestamp: Number(id)
+                }, { merge: true });
+            }
+            
+        })
+    })
+    
 }
   
 window.initMap = initMap;
